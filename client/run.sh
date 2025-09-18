3f96f27e8c5b8d9387f49495721ae1379b8a47b4
BaThong0 <legendfun276@gmail.com>
2025-09-18 22:47:53 +0700

    Config CMake of client to run on Window

--- client/run.sh
+++ client/run.sh
@@ -1,4 +1,12 @@
-cd "$(dirname "$(realpath "$0")")"
-cmake -S . -B build
-cmake --build build --target run --parallel
-pause
+#!/bin/bash
+BUILD_DIR=build
+
+# Step 1 - Configure
+cmake -S . -B $BUILD_DIR
+
+# Step 2 - Build Debug
+cmake --build $BUILD_DIR --target Spirit_Warrior_Client -- -j$(nproc)
+
+# Step 3 - Run
+echo "Running Spirit_Warrior_Client..."
+./$BUILD_DIR/Spirit_Warrior_Client
